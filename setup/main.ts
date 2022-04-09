1ed7c159d9ace8743d3e35b40bd818c42cd503b9
weisrc <weihengjiang64@hotmail.com>
2022-04-09 18:42:04 -0400

    added live

--- setup/main.ts
+++ setup/main.ts
@@ -1,6 +1,6 @@
 import { defineAppSetup } from "@slidev/types";
 import key from "./key.json";
-import liveSlidev from "live-slidev"
+import liveSlidev from "../live"
 import "@slidev/client/internals/Presenter.vue"
 
 const ROOM_ID = "ba1f51ab-8d96-44f6-8c94-f06d771b4e3a";
