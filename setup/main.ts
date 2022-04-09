c09ff7231836b31a997485486ff86874c119ee1a
weisrc <weihengjiang64@hotmail.com>
2022-04-09 16:45:03 -0400

    importing Presenter vue

--- setup/main.ts
+++ setup/main.ts
@@ -1,10 +1,11 @@
 import { defineAppSetup } from "@slidev/types";
 import key from "./key.json";
+import liveSlidev from "live-slidev"
+import "@slidev/client/internals/Presenter.vue"
 
 const ROOM_ID = "ba1f51ab-8d96-44f6-8c94-f06d771b4e3a";
 
 export default defineAppSetup(async (ctx) => {
-    const {init} = await import("live-slidev")
     const ws = new WebSocket("wss://rooms.deno.dev/" + ROOM_ID);
-	init(ctx, ws, key);
+	liveSlidev(ctx, ws, key);
 });
