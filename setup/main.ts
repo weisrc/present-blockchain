722b479bc3a82eeaf7013b5dcd5f675a1dafe347
weisrc <weihengjiang64@hotmail.com>
2022-04-09 16:17:52 -0400

    async import for main

--- setup/main.ts
+++ setup/main.ts
@@ -1,10 +1,10 @@
 import { defineAppSetup } from "@slidev/types";
-import liveSlidev from "live-slidev";
 import key from "./key.json";
 
 const ROOM_ID = "ba1f51ab-8d96-44f6-8c94-f06d771b4e3a";
 
-export default defineAppSetup((ctx) => {
+export default defineAppSetup(async (ctx) => {
+    const {init} = await import("live-slidev")
     const ws = new WebSocket("wss://rooms.deno.dev/" + ROOM_ID);
-	liveSlidev(ctx, ws, key);
+	init(ctx, ws, key);
 });
